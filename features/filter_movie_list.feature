18b3b68580a5953cb172b38fdb988a9e703e97f0
rhuberdeau <rhuberdeau@gmail.com>
2013-08-06 11:09:53 -0400

    at 93%

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -20,16 +20,25 @@ Background: movies have been added to database
   | Chicken Run             | G      | 21-Jun-2000  |
 
   And  I am on the RottenPotatoes home page
-  
+ 
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  Given I uncheck the following ratings: G,PG-13,NC-17
   # enter step(s) to uncheck all other checkboxes
+  And I check the following ratings: PG,R
   # enter step to "submit" the search form on the homepage
+  And I click the "Refresh" button
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see all the PG and R movies
   # enter step(s) to ensure that other movies are not visible
-
+  And I should not see any of the G, PG-13, and NC-17 movies
+  
 Scenario: no ratings selected
-  # see assignment
+  Given I uncheck all the ratings
+  And I click the "Refresh" button
+  Then I should see all the movies
 
 Scenario: all ratings selected
-  # see assignment
+  Given I check all the ratings
+  And I click the "Refresh" button
+  Then I should see all the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,25 +4,67 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(title: movie['title'], rating: movie['rating'], release_date: movie['release_date'])
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+Then /I should see "(.*)" before "(.*)"/ do |movie1, movie2|
+  texts = page.all("table#movies td").collect(&:text)
+  texts.length.should be > 0, "#{texts}"
+  texts.index(movie1).should be < texts.index(movie2), "#{texts}"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+Given /^I check the following ratings: (.*)$/ do |rating_list|
+  rating_list.split(",").each do |rating|
+    page.check "ratings_#{rating}"
+  end
+end
+
+When /I uncheck the following ratings: (.*)/ do |rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(",").each do |rating|
+    page.uncheck "ratings_#{rating}"
+  end
+end
+
+Given /^I click the "(.*?)" button$/ do |button|
+  page.click_button(button)
+end
+
+Then /^I should see all the PG and R movies$/ do
+  page.should have_selector('tr', text: "")
+end
+
+Then /^I should not see any of the G, PG\-(\d+), and NC\-(\d+) movies$/ do |arg1, arg2|
+  page.should_not have_content "Aladdin"
+  page.should_not have_content "Chocolat"
+end
+
+Given /^I uncheck all the ratings$/ do
+  page.uncheck "ratings_G"
+  page.uncheck "ratings_PG"
+  page.uncheck "ratings_R"
+  page.uncheck "ratings_PG-13"
+  page.uncheck "ratings_NC-17"
+end
+
+Given /^I check all the ratings$/ do
+  page.check "ratings_G"
+  page.check "ratings_PG"
+  page.check "ratings_R"
+  page.check "ratings_PG-13"
+  page.check "ratings_NC-17"
+end
+
+Then /^I should see all the movies$/ do
+  page.all('tr').length.should equal(11)
 end
